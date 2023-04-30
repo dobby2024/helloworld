091fff9bc0c672a50aca0dca09bca9b2bf4040ab
ITSC <ITSC@test.com>
2023-04-30 12:15:10 +0900

    homework

--- ex12-02-array3d.c
+++ ex12-02-array3d.c
@@ -27,6 +27,22 @@ int main(void)
         }
     };
 
+    // 3차 배열 전체 출력
+    int i, j, k;
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            for (k = 0; k < 4; k++)
+            {
+                printf("%d ", arr[i][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    
+
     
     return 0;
 }
