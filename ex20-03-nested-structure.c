ae49bf1ea7340237d535212a656e717f4a322512
ITSC <ITSC@test.com>
2023-05-14 11:51:52 +0900

    20

--- ex20-03-nested-structure.c
+++ ex20-03-nested-structure.c
@@ -23,12 +23,12 @@ int main(void)
     구조체 변수를 선언과 동시에 초기화
     이때 멤버 변수 또한 선언과 동시에 초기화
     */
-   Bag myBag = {
+     Bag myBag = {
         {"지금 하지 않으면 언제 하겠는가", 2018}
-   };
+    };
 
-   // 멤버 변수로써의 구조체 변수에 접근할 때에도 사용 연산자는 동일하다.
-   printf("책 제목 : %s \n 출간년도 : %d년\n", myBag.book.title, myBag.book.published);
+    // 멤버 변수로써의 구조체 변수에 접근할 때에도 사용 연산자는 동일하다.
+    printf("책 제목 : %s \n 출간년도 : %d년\n", myBag.book.title, myBag.book.published);
 
     return 0;
 }
